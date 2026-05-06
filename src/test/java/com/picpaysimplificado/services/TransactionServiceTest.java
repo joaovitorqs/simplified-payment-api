ce2ecffe9155ce50a5d5dc064a72713e6aab82ec
João Vitor <joao_vqs@hotmail.com>
2026-05-05 23:35:32 -0300

    Config mock in TrasactionServiceTest

---
+++ src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
@@ -0,0 +1,47 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.repositories.TransactionRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private TransactionRepository repository;
+
+    @Mock
+    private AuthorizationService authService;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Autowired
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Deve criar uma transação com sucesso")
+    void createTransactionCase01() {
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando a transação não for autorizada")
+    void createTransactionCase02() {
+    }
+}
