1e4a2bdfc869241ceb73fd4d0747f494aa988f50
João Vitor <joao_vqs@hotmail.com>
2026-05-06 01:11:02 -0300

    Update TrasactionServiceTest

--- src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
@@ -1,5 +1,8 @@
 package com.picpaysimplificado.services;
 
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransactionRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +13,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class TransactionServiceTest {
 
@@ -37,7 +44,30 @@ class TransactionServiceTest {
 
     @Test
     @DisplayName("Deve criar uma transação com sucesso")
-    void createTransactionCase01() {
+    void createTransactionCase01() throws Exception {
+        User sender = new User(1L, "João", "Santos", "99999999901", "joao@gmail.com", "1234", new BigDecimal(10),UserType.COMMON);
+        User receiver = new User(2L, "Pedro", "Queiroz", "99999999902", "pedro@gmail.com", "1234", new BigDecimal(10),UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+
+        when(authService.authorizeTransaction(any(), any())).thenReturn(true);
+
+        TransactionDTO transactionDTO = new TransactionDTO(new BigDecimal(10), 1L, 2L);
+        transactionService.createTransaction(transactionDTO);
+
+        verify(repository, times(1)).save(any());
+
+        sender.setBalance(new BigDecimal(0));
+        verify(userService, times(1)).saveUser(sender);
+
+        receiver.setBalance(new BigDecimal(20));
+        verify(userService, times(1)).saveUser(receiver);
+
+        verify(notificationService, times(1)).sendNotification(sender, "Transação realizada com sucesso");
+        verify(notificationService, times(1)).sendNotification(receiver, "Transação recebida com sucesso");
+
+
     }
 
     @Test
